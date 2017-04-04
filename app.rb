32efb259d1d7b6b989f27ce39cca28e18cd06bf6
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-04 17:09:32 +0800

    added actors.erb movies.erb, linkage working

--- app.rb
+++ app.rb
@@ -6,3 +6,47 @@ require("./lib/movie")
 require("pg")
 
 DB = PG.connect({:dbname => "movie_database"})
+
+get('/') do
+  @actors = Actor.all()
+  @movies = Movie.all()
+  erb(:index)
+end
+
+get("/actors") do
+  @actors=Actor.all()
+  erb(:actors)
+end
+
+get("/movies") do
+  @movies = Movie.all()
+  erb(:movies)
+end
+
+post("/actors") do
+  name = params.fetch("name")
+  actor = Actor.new({:name => name, :id => nil})
+  actor.save()
+  @actors = Actor.all()
+  erb(:actors)
+end
+
+post("/movies") do
+  name = params.fetch("name")
+  movie = Movie.new({:name => name, :id => nil})
+  movie.save()
+  @movies = Movie.all()
+  erb(:movies)
+end
+
+get("/actors/:id") do
+  @actor = Actor.find(params.fetch("id").to_i())
+  @movies = Movie.all()
+  erb(:actor_info)
+end
+
+get("/movies/:id") do
+  @movie = Movie.find(params.fetch("id").to_i())
+  @actors = Actor.all()
+  erb(:movie_info)
+end
