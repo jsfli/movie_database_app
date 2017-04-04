d67ab773afd59c48c7a42f5c2bd48dc4d4bea919
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-04 18:00:43 +0800

    gone through end of wednesday material, got everything working

--- app.rb
+++ app.rb
@@ -50,3 +50,21 @@ get("/movies/:id") do
   @actors = Actor.all()
   erb(:movie_info)
 end
+
+patch("/actors/:id") do
+  actor_id = params.fetch("id").to_i()
+  @actor = Actor.find(actor_id)
+  movie_ids = params.fetch("movie_ids")
+  @actor.update({movie_ids => movie_ids})
+  @movies=Movie.all()
+  erb(:actor_info)
+end
+
+patch("/movies/:id") do
+  movie_id = params.fetch("id").to_i()
+  @movie = Movie.find(movie_id)
+  actor_ids = params.fetch("actor_ids")
+  @movie.update({:actor_ids => actor_ids})
+  @actors = Actor.all()
+  erb(:movie_info)
+end
